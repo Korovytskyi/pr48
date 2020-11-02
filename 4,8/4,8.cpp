43982367a83ef91d00598ceaf0e335ce8d3982fc
Taras <taras.korovytskyi.itis.2020@lpnu.ua>
2020-11-02 13:22:12 +0200

    4,8

---
+++ 4,8/4,8.cpp
@@ -0,0 +1,38 @@
+﻿// 4.8.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>;
+
+using namespace std;
+
+int main()
+{
+    char a;
+    int x, y, c;
+    int q, w, e, r;
+    cout << "1";cin >> x;
+    cout << "2";  cin >> y;
+    cout << "";cin >> a;
+    if (a = '+')
+    {
+        q = x + y;
+        cout << "" << q << endl;
+    }
+    else if (a = '-')
+    {
+        w = x - y;
+        cout << "" << w << endl;
+    }
+    else if (a = '*')
+    {
+        e = x * y;
+        cout << "" << e << endl;
+    }
+    else if
+        (a = '/')
+    {
+        r = x / y;
+        cout << "" << r << endl;
+    }
+
+    return 0;
+}
